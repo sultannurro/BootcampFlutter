500cec33dadf4bab26412e847b3c8d0d254365ad
Muhammad Sultan Nurrochman <sultan.nurrochman@gmail.com>
2024-01-17 22:42:44 +0700

    Pengumpulan Tugas 3

---
+++ Tugas-3/Tugas-3A-Looping/looping.dart
@@ -0,0 +1,48 @@
+// Soal No 1
+// void main(){
+//     print("=======Soal No 1=======");
+//     var i = 2;
+//     var j = 20;
+//     print("#### Looping Pertama ####");
+//     while(i <= 20){
+//         print(i.toString()+" - I love coding");
+//         i = i + 2;
+//     }
+//     print("#### Looping Kedua ####");
+//     while(j >= 2){
+//         print(j.toString()+" - I will become a mobile developer");
+//         j = j - 2;
+//     }
+// }
+
+// Soal No 2
+// void main(){
+//     for(var i = 1; i <= 20; i++){
+//         if(i % 2 != 0 && i % 3 != 0){
+//             print(i.toString()+" - Santai");
+//         }else if(i % 2 == 0){
+//             print(i.toString()+" - Berkualitas");
+//         }else if(i % 2 != 0 && i % 3 == 0){
+//             print(i.toString()+" - I love coding");
+//         }
+//     }
+// }
+
+// Soal No 3
+// void main() {
+//     print("=======Soal No 3========");
+//     for (int i = 0; i < 4; i++) {
+//         print("# " * 8);
+//     }
+// }
+
+// Soal No 4
+// void main() {
+//     print("=======Soal No 4========");
+//     for (int i = 0; i < 1; i++) {
+//         for (int j = 0; j <= 7; j++) {
+//         print("# " * j);
+//         }
+//         print(""); 
+//     }
+// }
---
+++ Tugas-3/Tugas-3B-Function/function.dart
@@ -0,0 +1,60 @@
+// Soal No 1
+
+// teriak(){
+//     return "Hallo Sanbers!";
+// }
+// void main(){
+//     print("======= Soal No 1 =======");
+//     print(teriak());
+
+// }
+
+// Soal No 2
+
+// kalikan(a,b){
+//     return a*b;
+// }
+
+// void main(){
+//     var num1 = 12;
+//     var num2 = 4;
+//     var hasilKali = kalikan(num1, num2);
+//     print(hasilKali);
+// }
+
+// Soal No 3
+
+// introduce(name, age, address, hobby){
+//     return "Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!";
+// }
+// void main(){
+//     final String name = "Achmad";
+//     final int age = 30;
+//     final String address = "Jln. Malioboro, Yogyakarta";
+//     final String hobby = "Gaming";
+    
+//     final perkenalan = introduce(name, age, address, hobby);
+//     print(perkenalan);
+// }
+
+// Soal No 4
+// import 'dart:io';
+// factorial(x){
+//     int fact = 1;
+//     if(x <= 1){
+//         return 1;
+//     }else{
+//         for(var i = 1; i <= x; i++){
+//             fact *= i;
+//         }
+//         return fact;
+//     }
+    
+// }
+// void main(){
+//     print("======= Soal No 4 =======");
+//     stdout.write("Masukkan Angka: ");
+//     int angka = int.parse(stdin.readLineSync()!);
+//     print("Hasil faktorial dari $angka: ${factorial(angka)}");
+
+// }
