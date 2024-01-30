e60aea4aec3985102d70f0040d2ae78932d3a946
Muhammad Sultan Nurrochman <sultan.nurrochman@gmail.com>
2024-01-30 22:14:54 +0700

    last commit

--- lookerapp/lib/tugas/tugas10/home_screen.dart
+++ lookerapp/lib/tugas/tugas10/home_screen.dart
@@ -6,60 +6,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[400],
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.extension)),
-              ],
-            ),
-            SizedBox(height: 37),
-
-            // Teks sambutan
-            Text.rich(
-              TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "Hello, Sultan! \n",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextSpan(
-                    text: "Let’s Find Your Dream Jobs!",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            // Kotak pencarian
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.search,
-                ),
-                hintText: "Search a job or position...",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
-      ),
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: ClipPathClass,
+            child: Container(
+              height: 250,
+              width: 250,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF000114), Color(0xFF0075FF)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                )
+            )
+          )
+          )
+        ]
+      )
     );
   }
+}
+class ClipPathClass extends CustomClipper<Path>{
+  @override
+  var path = Path();
+  path.lineTo(0.0, )
 }
