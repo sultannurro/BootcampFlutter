dd80e6bbd511fb34c1754412f82c8c1ec5b42590
Muhammad Sultan Nurrochman <sultan.nurrochman@gmail.com>
2024-02-02 00:54:26 +0700

    Pengumpulan Tugas 14

---
+++ sanberappflutter/lib/Latihan/Latihan-1/services/services.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+import 'package:sanberappflutter/Latihan/Latihan-1/model/postmodel.dart';
+import 'dart:convert' as convert;
+
+class Services {
+  Future<List<PostModel>?> getAllPosts() async {
+    try {
+      var response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'))
+          .timeout(const Duration(seconds: 10), onTimeout: () {
+        throw TimeoutException('Connection time out. Try again.');
+      });
+
+      if (response.statusCode == 200) {
+        List jsonResponse = convert.jsonDecode(response.body);
+        return jsonResponse.map((e) => new PostModel.fromJson(e)).toList();
+      } else {
+        return null;
+      }
+    } on TimeoutException catch (_) {
+      print('Response time out.');
+    }
+  }
+}
